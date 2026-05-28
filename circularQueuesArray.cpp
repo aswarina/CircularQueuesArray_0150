c1cfdc43cabb94302a93191cec6f3fcb3074fe93
aswarina <lyxsyyawa@gmail.com>
2026-05-28 11:32:22 +0700

    nambahin print elemen yang dihapus

--- circularQueuesArray.cpp
+++ circularQueuesArray.cpp
@@ -54,4 +54,5 @@ class Queues
             cout << "Queue underflow\n";
             return;
         }
+        cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
 }
